2449305f26e889993bcc87cff46e64c2d2b2ee63
Lucchetta <lucchettan@gmail.com>
2019-03-12 13:57:05 +0100

    CRUD in controller

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,2 +1,16 @@
 class GardensController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def destroy
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+    def show
+        @userid = current_session.id
+    end
+    
+    def create
+    end
+
+    def edit
+    end
+
+    def destroy
+    end
+
+    private
+
+    def correct_user
+        @user = User.find(params[:id].to_i)
+        if current_user != @user
+          flash[:alert] = "This profile is not accessible"
+          redirect_to root_path
+        end
+    end 
 end
