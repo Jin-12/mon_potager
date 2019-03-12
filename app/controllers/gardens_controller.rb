c8e67bd7fc8e83d74dc385c4f28fe98c73603b2a
Lucchetta <lucchettan@gmail.com>
2019-03-12 14:55:17 +0100

    show/index done for user & garden controllers

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -3,12 +3,15 @@ class GardensController < ApplicationController
     end
 
     def show
+        @garden = Garden.find_by(user_id: current_session.id)
+        @products = @garden.products
     end
 
     def create
     end
 
     def edit
+        @garden = Garden.find_by(user_id: current_session.id)
     end
 
     def destroy
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 User.create(email: "kiko@kiko.com", age: 21, description: "un humble serviteur", password: "kikokiko", first_name: "Nicolas", last_name: "Lucchetta")
 User.create(email: "clem@clem.com", age: 21, description: "un humble penseur", password: "clemclem", first_name: "Clement", last_name: "Kuzniewycz")
 User.create(email: "aurel@aurel.com", age: 21, description: "un humble designer", password: "aurelaurel", first_name: "Aurelien", last_name: "Verschueren")
-User.create(email: "jul@jul.com", age: 21, description: "bun humble analyste", password: "juljul", first_name: "Jules", last_name: "outureaud")
+User.create(email: "jul@jul.com", age: 21, description: "bun humble analyste", password: "juljul", first_name: "Jules", last_name: "Couturaud")
 User.create(email: "cam@cam.com", age: 21, description: "une humble marketeuse", password: "camcam", first_name: "Camille", last_name: "Bideaux")
 User.create(email: "oliv@oliv.com", age: 21, description: "une force tranquille", password: "olivoliv", first_name: "Olivier", last_name: "Cardinaux")
 
