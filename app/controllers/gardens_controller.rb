95839c58c1c50487be74a75ce1501f4df8acd074
Aurelien Verschueren <aurelienverschueren@gmail.com>
2019-03-12 20:30:58 +0100

    1ere partie page show garden

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,25 +1,22 @@
 class GardensController < ApplicationController
-<<<<<<< HEAD
-  def show
-  end
-=======
     def index
     end
 
     def show
-        @garden = Garden.find_by(user_id: current_session.id)
+        @garden = Garden.find_by(id: params[:id])
         @products = @garden.products
+        @status = Status.find_by(user_id: current_user.id)
+        @how_many_days = (Time.now.to_i - @status.updated_at.to_i) / 86400
+        @harvest = Harvest.where(garden_id: @garden)
     end
 
     def create
     end
 
     def edit
-        @garden = Garden.find_by(user_id: current_session.id)
+        @garden = Garden.find_by(user_id: current_user.id)
     end
 
     def destroy
     end
-
->>>>>>> aurelien_delivery
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( style.css )
+Rails.application.config.assets.precompile += %w( application.css.scss )
+Rails.application.config.assets.precompile += %w( gardens.css )
+Rails.application.config.assets.precompile += %w( vendor )
