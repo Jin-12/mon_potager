345b78c08daf6a4648324c03c873b020fefdabbc
Lucchetta <lucchettan@gmail.com>
2019-03-13 14:37:45 +0100

    CRUD started on controllers

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,13 +1,21 @@
 class GardensController < ApplicationController
     def index
+        @gardens = Garden.all
     end
 
     def show
         @garden = Garden.find_by(user_id: current_user.id)
         @products = @garden.products
+        @comments = Comment.where(commentable_id: params[:id])
+        @favuser = Favorite.where(favoritable_id: params[:id])
     end
 
     def create
+        Garden.create(user_id: current_user.id, name: params[:gardenname], adress: params[:adress])
+        if Product.create(name: params[:productname]) do
+        Harvest.create(product_id: (Product.last)id, garden_id: (Garden.last)id)
+        end
+        redirect_to root_path
     end
 
     def edit
@@ -16,5 +24,4 @@ class GardensController < ApplicationController
 
     def destroy
     end
-
 end
