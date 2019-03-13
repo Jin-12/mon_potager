29c9e977954d19158516d751ee3b2df6025390f6
OcardinauX <ocardinaux@yahoo.fr>
2019-03-13 15:08:07 +0100

    changes to display of garden maps and lists

--- app/services/generate_map_for_index.rb
+++ app/services/generate_map_for_index.rb
@@ -18,6 +18,7 @@ class GenerateMapForIndex
       marker.lat garden.latitude
       marker.lng garden.longitude
       marker.json({title: garden.name})
+      marker.title garden.name
       marker.picture({
           "url": ac.helpers.image_url('carrot.png'),
           "width": 32,
