89f910f15cb41dc46737394974927da658c8f71e
OcardinauX <ocardinaux@yahoo.fr>
2019-03-18 11:36:31 +0100

    puts infobox back in map on index

--- app/services/generate_map_for_index.rb
+++ app/services/generate_map_for_index.rb
@@ -23,7 +23,7 @@ class GenerateMapForIndex < ApplicationController
         "width": 32,
         "height": 32
       )
-      marker.infowindow render_to_string(partial: 'gardens/map_info', locals: { garden: garden}, object: garden)
+      marker.infowindow render_to_string(partial: 'gardens/map_info', locals: { garden: garden}, object: @garden)
       rescue
         marker.lat garden.latitude
         marker.lng garden.longitude
@@ -32,6 +32,7 @@ class GenerateMapForIndex < ApplicationController
           "width": 32,
           "height": 32
         )
-        end
+        marker.infowindow render_to_string(partial: 'gardens/map_info', locals: { garden: garden })
+      end
   end
 end
