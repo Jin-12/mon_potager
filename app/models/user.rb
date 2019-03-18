6d5ea2dd293529f4d8ee1e20971991b049ac1f43
KuzniClem <clement@kuzniewycz.fr>
2019-03-18 14:30:28 +0100

    callback after create user mailer welcome mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  after_create :welcome_send
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -27,4 +28,8 @@ class User < ApplicationRecord
   # end
 
   has_one_attached :avatar
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
