098f879b8b2a5bfad5e2961c76dd016f642d48fd
KuzniClem <clement@kuzniewycz.fr>
2019-03-18 16:20:41 +0100

    routes reduction and comments destroy method change find with garden_id to find with the actual comment_id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,8 +4,8 @@ class CommentsController < ApplicationController
   def index; end
 
   def create
-    Comment.create(garden_id: params[:id], content: params[:content], user_id: current_user.id)
-    redirect_to gardens_path(params[:id])
+    Comment.create(garden_id: params[:garden_id], content: params[:content], user: current_user)
+    redirect_to gardens_path(params[:garden_id])
   end
 
   def show
@@ -13,9 +13,7 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    puts 'Ooo_________________ca ne fonctionne passsss ___________________________________-ooO'
     Comment.find(params[:id]).destroy
-    puts '____________________________________________________________'
     redirect_to gardens_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # concern :commentable do
-  #   resources :comments
-  # end
+  concern :commentable do
+    resources :comments
+  end
+
+  concern :image_attachable do
+    resources :images, only: %i[create destroy]
+  end
 
   root to: 'gardens#index'
   devise_for :users
@@ -11,12 +15,8 @@ Rails.application.routes.draw do
     resources :avatars, only: %i[create destroy]
     resources :statuses
   end
-  resources :gardens do
-    resources :images, only: %i[create destroy]
-    resources :comments
-  end
+  resources :gardens, concerns: [:commentable, :image_attachable]
   resources :favorites, only: [:create]
-  resources :comments
 
   get "/static/landing", to: "static#landing"
   get "/static/landing_map", to: "static#landing_map"
