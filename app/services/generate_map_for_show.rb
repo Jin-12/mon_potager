01f3b29d3fb885797c9dccf27cf557d5fc18622b
Olivier Cardinaux <ocardinaux@yahoo.fr>
2019-03-19 13:03:52 +0100

    still trying

--- app/services/generate_map_for_show.rb
+++ app/services/generate_map_for_show.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class GenerateMapForShow < ApplicationController
+  include Rails.application.routes.url_helpers
+
   def initialize(garden, nearby)
     @garden = garden
     @nearby = nearby
@@ -14,6 +16,7 @@ class GenerateMapForShow < ApplicationController
 
   def build_hash
     @hash_nearby = Gmaps4rails.build_markers(@nearby) do |garden, marker|
+      @garden_img = Rails.application.routes.url_helpers.rails_blob_path(garden.images[0], :host)
       marker.lat garden.latitude
       marker.lng garden.longitude
       marker.json(id: garden.id)
@@ -24,13 +27,12 @@ class GenerateMapForShow < ApplicationController
         "width": 32,
         "height": 32
       )
-      marker.infowindow render_to_string(partial: 'gardens/map_info_show', locals: { garden: garden })
+      marker.infowindow render_to_string(partial: 'gardens/map_info_show', locals: { garden: garden, garden_img: @garden_img })
     end
     @hash_garden = Gmaps4rails.build_markers([@garden]) do |garden, marker|
       marker.lat garden.latitude
       marker.lng garden.longitude
-      marker.json({ id: garden.id })
-      marker.json(title: garden.name)
+      marker.json({ id: garden.id, title: garden.name })
       marker.title garden.name
       marker.picture(
         "url": helpers.image_url('carrot_lg.png'),
