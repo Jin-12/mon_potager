618f0b188bbf4a3dd8434c18d02f7a818049bb9e
OcardinauX <ocardinaux@yahoo.fr>
2019-03-19 17:53:25 +0100

    install and setup mail_form gem for contacts

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
   resources :favorites
   resources :comments
 
+  resources "contacts", only: [:new, :create]
+  
   root to: 'gardens#index'
 
   get "/static/landing", to: "static#landing"
