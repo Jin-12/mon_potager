755684e9c8cf7043789ea6fcb8b3fb141e81c2b7
Kamiyechung <camille.bideau@gmail.com>
2019-03-21 14:16:57 +0100

    Merge pull request #74 from Jin-12/delivery_olivier
    Delivery olivier

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    user_path(current_user)
+    user_path(current_user, {status: "new"})
   end
 end
