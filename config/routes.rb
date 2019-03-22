d645dc7744cb068f27b445a3f1c1726529eba0a7
Kamiyechung <camille.bideau@gmail.com>
2019-03-22 11:28:51 +0100

    cgu fixed

--- config/routes.rb
+++ config/routes.rb
@@ -29,5 +29,5 @@ Rails.application.routes.draw do
   get "/static/landing_map", to: "static#landing_map"
   get "/static", to: "static#landing"
   get "/static/about_us", to: "static#about_us"
-  get "/static/cgu", to: "static#cgu"
+  get "/static/gtu", to: "static#gtu"
 end
